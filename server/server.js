71ed0ebd378b7a15084d07e6ead7f94f3490c227
Varidh Srivastav <varidhsrivastava19145@gmail.com>
2023-02-23 02:54:29 +0530

    updated server

---
+++ server/server.js
@@ -0,0 +1,25 @@
+const express = require("express");
+const dotenv = require("dotenv");
+const app = express();
+// app.use( cors())
+dotenv.config({path:'./.env'})
+require("./db/conn");
+const Donor = require("./models/donoeSchema")
+app.use(express.json());
+app.use(require('./routes/auth.js'))
+
+const PORT = process.env.PORT;
+
+const middleware = (req,res, next)=>{
+    console.log("middleware occures");
+    next();
+}
+
+
+  app.listen(PORT, () => {
+    console.log(`donationService listening http://localhost:${PORT}`);
+  });
+  
+
+
+//   mongodb+srv://varidhsrivastava:<password>@cleanclosetcluster.bcprnbj.mongodb.net/?retryWrites=true&w=majority
